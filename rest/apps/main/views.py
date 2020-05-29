dc9b2816e87cb49d9f8bcf2894b9d2501bd68628
jekajeka63 <jekajeka63@gmail.com>
2020-05-29 17:11:25 +0300

    make restorant page, make form add restorants

--- rest/apps/main/views.py
+++ rest/apps/main/views.py
@@ -5,3 +5,17 @@ from rest.apps.main.models import Restorant
 def index(request):
     restorants = Restorant.objects.all().order_by("-rating_restorant")
     return render(request,'index.html', {'restorants': restorants})
+
+def rest_detail(request,rest_detail):
+    name = Restorant.objects.get(id=rest_detail)
+    print(name)
+    return render(request,"rest_detail.html", {"name":name})
+
+def form(request):
+    s = request.POST.dict()
+    data = Restorant(name_restorant = s.get("name_restorant"),
+                     rescription_restorant = s.get("rescription_restorant"),
+                     rating_restorant= int(s.get("rating_restorant")))
+    data.save()
+
+    return HttpResponse(s.get("name_restorant"))
