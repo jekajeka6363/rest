27b98e1bc06f25b83fdc3ed85e31844af379b153
jekajeka63 <jekajeka63@gmail.com>
2020-05-29 13:28:13 +0300

    fix buggs

--- rest/apps/main/views.py
+++ rest/apps/main/views.py
@@ -3,5 +3,5 @@ from rest.apps.main.models import Restorant
 
 # Create your views here.
 def index(request):
-    restorants = Restorant.objects.all()
+    restorants = Restorant.objects.all().order_by("-rating_restorant")
     return render(request,'index.html', {'restorants': restorants})
